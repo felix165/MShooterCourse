2d572a060b97d6c69af3e479cf5465a565765f40
felix165 <felixliem83@gmail.com>
2025-02-22 21:59:03 +0700

    39 Movement Added

--- Source/MShooterCourse/Character/MSCharacter.cpp
+++ Source/MShooterCourse/Character/MSCharacter.cpp
@@ -25,15 +25,55 @@ void AMSCharacter::BeginPlay()
 	
 }
 
+
+void AMSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+
+	PlayerInputComponent->BindAxis("Move Forward / Backward", this, &AMSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Move Right / Left", this, &AMSCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn Right / Left Mouse", this, &AMSCharacter::Turn);
+	PlayerInputComponent->BindAxis("Look Up / Down Mouse", this, &AMSCharacter::LookUp);
+}
+
+void AMSCharacter::MoveForward(float Value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("MoveForward: %f"), Value);
+	if(Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMSCharacter::MoveRight(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMSCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+
+}
+
+void AMSCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
 void AMSCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-void AMSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
