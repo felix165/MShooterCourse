fdcd7faa6b0c26c18cf83c1f5df48a9072ede647
felix165 <felixliem83@gmail.com>
2025-02-28 15:44:14 +0700

    44. WeaponClass

---
+++ Source/MShooterCourse/Character/Weapon.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial UMETA(DisplayName = "Initial State"),
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+	EWS_MAX UMETA(DisplayName = "DefaultMAX"),
+};
+
+UCLASS()
+class MSHOOTERCOURSE_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AWeapon();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Weapon")
+	USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon")
+	class USphereComponent* AreaSphere;
+
+	UPROPERTY(VisibleAnywhere)
+	EWeaponState WeaponState;
+
+public:
+};
