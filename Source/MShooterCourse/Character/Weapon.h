5b69d19a1f8d85791f070c8f7dd5753226dd35d8
felix165 <felixliem83@gmail.com>
2025-03-01 11:25:36 +0700

    45. Pickup Widget

--- Source/MShooterCourse/Character/Weapon.h
+++ Source/MShooterCourse/Character/Weapon.h
@@ -26,6 +26,7 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 public:	
