ba31d7cff6d5c17b44097e05dd9e7c907921c268
felix165 <felixliem83@gmail.com>
2025-03-08 12:36:28 +0700

    59. Pitch in Multiplayer

--- Source/MShooterCourse/Character/MSCharacter.cpp
+++ Source/MShooterCourse/Character/MSCharacter.cpp
@@ -196,6 +196,13 @@ void AMSCharacter::AimOffset(float DeltaTime)
 	}
 
 	AO_Pitch = GetBaseAimRotation().Pitch;
+	if (AO_Pitch > 90.f && !IsLocallyControlled())
+	{
+		// map pitch from [270, 360) to [-90, 0)
+		FVector2D InRange(270.f, 360.f);
+		FVector2D OutRange(-90.f, 0.f);
+		AO_Pitch = FMath::GetMappedRangeValueClamped(InRange, OutRange, AO_Pitch);
+	}
 }
 
 void AMSCharacter::ServerEquipButtonPressed_Implementation()
